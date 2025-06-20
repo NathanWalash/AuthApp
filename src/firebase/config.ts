09ffdb54c072b4a9ee5a5bd097338501fb457ac5
Nathan Walsh <nwalsh03@icloud.com>
2025-06-20 13:22:02 +0100

    externalize firebase config to .env

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,21 +1,29 @@
 // src/firebase/config.ts
 import { initializeApp } from 'firebase/app';
-import { initializeAuth, getReactNativePersistence} from 'firebase/auth';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import Constants from 'expo-constants';
 
+const {
+  firebaseApiKey,
+  firebaseAuthDomain,
+  firebaseProjectId,
+  firebaseStorageBucket,
+  firebaseMessagingSenderId,
+  firebaseAppId,
+} = (Constants.expoConfig?.extra ?? {}) as Record<string, string>;
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAeFDQiFs7vhHn54hDyU4El3eZ78gth9Oo',
-  authDomain: 'web3-mobile-auth-starter.firebaseapp.com',
-  projectId: 'web3-mobile-auth-starter',
-  storageBucket: 'web3-mobile-auth-starter.appspot.com',
-  messagingSenderId: '360323243142',
-  appId: '1:360323243142:web:5b6dbd519be717f7008507',
+  apiKey: firebaseApiKey,
+  authDomain: firebaseAuthDomain,
+  projectId: firebaseProjectId,
+  storageBucket: firebaseStorageBucket,
+  messagingSenderId: firebaseMessagingSenderId,
+  appId: firebaseAppId,
 };
 
 const app = initializeApp(firebaseConfig);
 
-// this replaces getAuth(app)
 export const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(AsyncStorage),
 });
